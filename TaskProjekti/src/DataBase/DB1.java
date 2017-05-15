3955e71576efe4f5b601fc0d58f70e26d5335d9c
Osku Sirpoma <osku.sirpoma@gmail.com>
2017-05-15 14:24:46 +0300

    -

---
+++ TaskProjekti/src/DataBase/DB1.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author osku0
+ */
+public class DB1 {
+    
+    private Connection conn;
+    public DB1(String urlToDataBase) throws ClassNotFoundException, SQLException {
+
+        String driver = "net.ucanaccess.jdbc.UcanaccessDriver";
+        Class.forName(driver);
+        conn = DriverManager.getConnection("jdbc:ucanaccess://Task.accdb");
+        System.out.println("Connected");
+    }
+
+    public DB1() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public ResultSet query(String SQL) throws SQLException {
+        Statement stmt = conn.createStatement();
+        ResultSet result = stmt.executeQuery(SQL);
+        return result;
+    }
+
+    public int updateReturnID(String SQL) throws SQLException {
+        Statement stmt = conn.createStatement();
+        int id = -1;
+        stmt.executeUpdate(SQL, Statement.RETURN_GENERATED_KEYS);
+        ResultSet result = stmt.getGeneratedKeys();
+        if (result.next()) {
+            id = result.getInt(1);
+        }
+        return id;
+    }
+}
